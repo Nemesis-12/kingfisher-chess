a1fa2a68b4fd5072e99907af3a356bfcda18ca90
NemesisTCO <mfa200312@gmail.com>
2025-02-04 22:49:04 -0600

    Removed function

--- player.py
+++ player.py
@@ -1,12 +1,7 @@
 class Player:
     def __init__(self):
-        self.turn = "White"
-        self.white_pieces = 0x000000000000FFFF
-        self.black_pieces = 0xFFFF000000000000
+        self.turn = True
 
     def player_turn(self):
-        self.turn = "Black" if self.turn == "White" else "White"
+        self.turn = not self.turn
         return self.turn
-    
-    def occupied_squares(self):
-        return self.white_pieces | self.black_pieces
