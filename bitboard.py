da0140cbe1f02c53fc6298c0a8ac435e31e97e3a
NemesisTCO <mfa200312@gmail.com>
2025-02-08 17:25:19 -0600

    Added co-ordinates list for mapping squares

--- bitboard.py
+++ bitboard.py
@@ -46,6 +46,18 @@ NOT_HG_FILE = ~FILE_H & ~FILE_G
 # Generate pre-computed squares
 SQUARES = [1 << square for square in range(64)]
 
+# Define co-ordinate map
+coord = [
+    "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
+    "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+    "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+    "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+    "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+    "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+    "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+    "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+]
+
 # Define actions that a player can take
 class Move():
     def __init__(self, square=None, is_resign=False):
