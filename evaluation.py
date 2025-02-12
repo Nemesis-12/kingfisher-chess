ef2e1719a307a79e05864b07884e386447f178c4
NemesisTCO <mfa200312@gmail.com>
2025-02-12 01:00:04 -0600

    Added basic evaluation function

---
+++ evaluation.py
@@ -0,0 +1,31 @@
+import chess
+
+# Evaluate the current board position and return who's winning
+def evaluation(board, curr_player):
+    # Initialize piece scores
+    piece_values = {
+        chess.PAWN: 1,
+        chess.KNIGHT: 3,
+        chess.BISHOP: 3,
+        chess.ROOK: 5,
+        chess.QUEEN: 9,
+        chess.KING: 0
+    }
+
+    # Initialize material count
+    material_count = 0
+
+    # Count the value of pieces on board and determine who is leading
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        
+        if piece:
+            value = piece_values[piece.piece_type]
+            if piece.color == curr_player.player:
+                material_count += value
+            else:
+                material_count -= value
+        
+    if curr_player.player == chess.BLACK:
+        return -material_count
+    return material_count
