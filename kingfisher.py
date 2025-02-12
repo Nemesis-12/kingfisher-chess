a12bacd96224dfd731e58a7e8b879cfd740fca0f
NemesisTCO <mfa200312@gmail.com>
2025-02-12 01:26:14 -0600

    Updated Kingfisher to use minimax search

--- kingfisher.py
+++ kingfisher.py
@@ -1,15 +1,32 @@
 import random
+import search
+from player import Player
+from evaluation import evaluation
 
+# Define the AI
 class Kingfisher():
-    def __init__(self):
-        pass
+    # Initialize
+    def __init__(self, max_depth=5):
+        self.max_depth = max_depth
 
+    # Select a move to play
     def select_move(self, board):
-        is_legal = list(board.legal_moves)
+        best_moves = []
+        best_score = float('-inf')
 
-        if not is_legal:
-            return None
-        
-        random_move = random.choice(is_legal)
+        # Check for legal moves and evaluate them
+        for move in board.legal_moves:
+            board.push(move)
 
-        return random_move
+            score = search.best_move(board, Player(board), self.max_depth -1, evaluation)
+
+            if score > best_score:
+                best_score = score
+                best_moves = [move]
+            elif score == best_score:
+                best_moves.append(move)
+
+            board.pop()
+
+        # Play the best move according to the current position
+        return random.choice(best_moves)
