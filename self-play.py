e0a1ea73239990d856436f6ba068275d86ce96ae
NemesisTCO <mfa200312@gmail.com>
2025-02-12 01:27:05 -0600

    Added Player and evaluation

--- self-play.py
+++ self-play.py
@@ -2,26 +2,15 @@ import chess
 from kingfisher import Kingfisher
 import time
 import chess.polyglot
+from player import Player
+from evaluation import evaluation
 
-board = chess.Board()
-
-bots = {
-    chess.WHITE: Kingfisher(),
-    chess.BLACK: Kingfisher()
-}
-
-piece_unicode = {
-    "r": "♜", "n": "♞", "b": "♝", "q": "♛", "k": "♚", "p": "♟",
-    "R": "♖", "N": "♘", "B": "♗", "Q": "♕", "K": "♔", "P": "♙", None: " . "
-}
-
-transposition_table = {}
-
-def print_move(player, move, board):
+def print_move(player, move, board, eval_function):
     piece = board.piece_at(move.from_square)
 
     player_name = "White" if player == chess.WHITE else "Black"
     print(f"{player_name} plays {piece_unicode[str(piece)]} {move.uci()[-2:]}")
+    print(f"Evaluation: {eval_function}")
 
 def print_board(board):
     print("    a   b   c   d   e   f   g   h")
@@ -39,22 +28,35 @@ def print_board(board):
 
     print("    a   b   c   d   e   f   g   h")
 
+board = chess.Board()
+
+bots = {
+    chess.WHITE: Kingfisher(),
+    chess.BLACK: Kingfisher()
+}
+
+piece_unicode = {
+    "r": "♜", "n": "♞", "b": "♝", "q": "♛", "k": "♚", "p": "♟",
+    "R": "♖", "N": "♘", "B": "♗", "Q": "♕", "K": "♔", "P": "♙", None: " . "
+}
+
+transposition_table = {}
+
 while not board.is_game_over():
     time.sleep(0.3)
 
     print_board(board)
 
-    player = chess.WHITE if board.turn else chess.BLACK
+    curr_player = Player(board)
 
-    bot_move = bots[player].select_move(board)
-    print_move(player, bot_move, board)
-
-    board.push(bot_move)
+    move = bots[curr_player.player].select_move(board)
+    board.push(move)
+    score = evaluation(board, curr_player)
+    
+    print_move(curr_player.player, move, board, score)
     print()
 
     zobrist_hash = chess.polyglot.zobrist_hash(board)
-
-    # Add the Zobrist hash to the transposition table
     if zobrist_hash not in transposition_table:
         transposition_table[zobrist_hash] = {
             "position_count": 1
@@ -65,7 +67,7 @@ while not board.is_game_over():
 print_board(board)
 
 if board.is_checkmate():
-        print(f"Checkmate! {player} wins!")
+    print(f"Checkmate! {curr_player.player_name} wins!")
 elif board.is_stalemate():
     print("Stalemate! It's a draw!")
 elif board.is_insufficient_material():
