1824c2ac806cb143b1fe9e2c199c87e6c1e8cb02
NemesisTCO <mfa200312@gmail.com>
2025-02-13 00:40:28 -0600

    Updated to use negamax search with alpha-beta pruning

--- kingfisher.py
+++ kingfisher.py
@@ -18,15 +18,16 @@ class Kingfisher():
         for move in board.legal_moves:
             board.push(move)
 
-            score = search.best_move(board, Player(board), self.max_depth -1, evaluation)
+            score = -search.negamax_search(board, Player(board), 
+                                           self.max_depth -1, evaluation)
+
+            board.pop()
 
             if score > best_score:
                 best_score = score
                 best_moves = [move]
             elif score == best_score:
                 best_moves.append(move)
-
-            board.pop()
 
         # Play the best move according to the current position
         return random.choice(best_moves)
