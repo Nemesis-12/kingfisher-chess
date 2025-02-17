bb3c9ed7b0c2d68ca1be278a9e05ed8740918d47
NemesisTCO <mfa200312@gmail.com>
2025-02-16 20:41:23 -0600

    Updated to use MCTS for searching

--- kingfisher.py
+++ kingfisher.py
@@ -1,33 +1,11 @@
-import random
-import search
-from player import Player
-from evaluation import evaluation
+from mcts import MCTS
 
 # Define the AI
 class Kingfisher():
     # Initialize
-    def __init__(self, max_depth=5):
-        self.max_depth = max_depth
+    def __init__(self, time_limit=5.0):
+        self.mcts = MCTS(max_time=time_limit)
 
     # Select a move to play
     def select_move(self, board):
-        best_moves = []
-        best_score = float('-inf')
-
-        # Check for legal moves and evaluate them
-        for move in board.legal_moves:
-            board.push(move)
-
-            score = -search.negamax_search(board, Player(board), 
-                                           self.max_depth -1, evaluation)
-
-            board.pop()
-
-            if score > best_score:
-                best_score = score
-                best_moves = [move]
-            elif score == best_score:
-                best_moves.append(move)
-
-        # Play the best move according to the current position
-        return random.choice(best_moves)
+        return self.mcts.search(board)
