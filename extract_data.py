e8919e0284d8450e12742ef61dafa9dd3033daa8
NemesisTCO <mfa200312@gmail.com>
2025-02-18 23:10:34 -0600

    Update to save as FEN and npz

--- extract_data.py
+++ extract_data.py
@@ -1,28 +1,25 @@
 import chess.pgn
-import pandas as pd
-from encoder import Encoder
 import multiprocessing as mp
+import numpy as np
 
 class DataExtractor:
     def __init__(self, file):
         self.file = file
-        self.encoder = Encoder()
 
     def process_game(self, game):
-        features = []
-        labels = []
+        fen, moves = [], []
         board = game.board()
         for move in game.mainline_moves():
-            features.append(self.encoder.encode(board).flatten())
-            labels.append(move.uci())
+            fen.append(board.fen())
+            moves.append(move.uci())
             board.push(move)
 
-        return features, labels
+        return fen, moves
     
     def extract_pgn(self, no_of_games=None, batch_size=100000):
         games_processed = 0
-        chunk_features = []
-        chunk_labels = []
+        fen_chunk = []
+        move_chunk = []
         pgn = open(self.file)
 
         with mp.Pool(mp.cpu_count()) as pool:
@@ -34,33 +31,25 @@ class DataExtractor:
 
                 results = pool.map(self.process_game, games)
 
-                for features, labels in results:
-                    chunk_features.extend(features)
-                    chunk_labels.extend(labels)
+                for fens, moves in results:
+                    fen_chunk.extend(fens)
+                    move_chunk.extend(moves)
 
                 games_processed += len(games)
 
-                if len(chunk_features) >= batch_size:
-                    feature_df = pd.DataFrame(chunk_features)
-                    label_df = pd.DataFrame(chunk_labels, columns=["Moves"])
+                if len(fen_chunk) >= batch_size:
+                    np.savez_compressed(f"fen_{games_processed}.npz", fens=fen_chunk, moves=move_chunk)
 
-                    feature_df.to_csv("features.csv", mode="a", index=False)
-                    label_df.to_csv("labels.csv", mode="a", index=False)
-
-                    chunk_features.clear()
-                    chunk_labels.clear()
+                    fen_chunk.clear()
+                    move_chunk.clear()
 
                 print(f"Processed {games_processed} games...")
 
                 if no_of_games is not None and games_processed >= no_of_games:
                     break
 
-        if chunk_features:
-            feature_df = pd.DataFrame(chunk_features)
-            label_df = pd.DataFrame(chunk_labels, columns=["Moves"])
-
-            feature_df.to_csv("features.csv", mode="a", index=False)
-            label_df.to_csv("labels.csv", mode="a", index=False)
+        if fen_chunk:
+            np.savez_compressed(f"fen_{games_processed}.npz", fens=fen_chunk, moves=move_chunk)
 
         print("Completed extraction")
 
